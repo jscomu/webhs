aa4a04bef127f2a849100bbad6a16252689736f8
KJS <comujs@gmail.com>
2024-08-04 22:30:01 +0900

    pc1

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,46 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          안녕! 그늘과 Edit <code>src/App.tsx</code> and<br />
-          save to reload.
-        </p>
-        <hr />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+    <header>
+        <p>Cover Title</p>
+        <ul>
+            <li><a href="#">Home1</a></li>
+            <li><a href="#bbb">Home2</a></li>
+            <li><a href="#ccc">Home3</a></li>
+        </ul>
+    </header>
+    <section id="aaa">
+        <p>Section 1111111 Title</p>
+        <p>Section 1111111 Title</p>
+        <img src="https://www.w3schools.com/w3images/avatar3.png" className="proPhoto" alt="Person" />
+        <p>Title Lorem ipusm sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+        <p>Lorem ipusm sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+
+        <a href="https://www.youtube.com/watch?v=LZ5M-e5z01c">Learn more</a>
+    </section>
+    <section id="bbb">
+        <p>Section 2 Title</p>
+        <p>가나다라111111111122222222222222222222 가나다라 cccc bbbb a</p>
+        <p>가나다라 가나다라 cccc bbbb a</p>
+        <p>가나다라111111111122222222222222222222 가나다라 cccc bbbb a</p>
+        <p>가나다라111111111122222222222222222222 가나다라 cccc bbbb a</p>
+        <p>가나다라 가나다라 cccc bbbb a</p>
+        <p>가나다라111111111122222222222222222222 가나다라 cccc bbbb a</p>
+        <p>가나다라 가나다라 cccc bbbb a</p>
+        <p>가나다라111111111122222222222222222222 가나다라 cccc bbbb a</p>
+        <p>가나다라 가나다라 cccc bbbb a</p>
+        <p>가나다라111111111122222222222222222222 가나다라 cccc bbbb a</p>
+        <p>가나다라 가나다라 cccc bbbb a</p>
+        <a href="https://www.youtube.com/watch?v=LZ5M-e5z01c">Learn more</a>
+    </section>
+    <section id="ccc">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3163.100452954271!2d126.94781206548213!3d37.55269703047339!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sko!2skr!4v1722414251352!5m2!1sko!2skr" width="600" height="450" loading="lazy"></iframe>
+    </section>
+    <footer>
+        reserved
+    </footer>
+</div>
   );
 }
 
